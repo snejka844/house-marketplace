bd14e34595203133f69c435f52d1216740da3b96
Snezhana <snezhana.stoyanova@icb.bg>
2022-10-26 17:29:02 +0300

    84. Sign In & Sign Up Forms 85. Register User 86. Save User To Firestore 87. User Sign In 88. Alerts With React Toastify 89. User Logout 90. Display & Update User Details 91. PrivateRoute Component & useAuthStatus Hook 92. Forgot Password Page 93. Google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,103 @@
+import { useState } from "react";
+import {toast} from 'react-toastify'
+import OAuth from '../components/OAuth'
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState)=>({
+        ...prevState,
+        [e.target.id]:e.target.value
+    }))
+  };
+
+  const onSubmit = async (e)=>{
+    e.preventDefault()
+
+    try {
+        const auth = getAuth()
+
+    const userCredential = await signInWithEmailAndPassword(auth,email,password)
+
+    if(userCredential.user){
+        navigate('/')
+    }
+    } catch (error) {
+        toast.error('Bad User Credentials')
+    }
+    
+  }
+
   return (
-    <h1>SignIn</h1>
-  )
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+        <main>
+          <form onSubmit={onSubmit}>
+            <input
+              type="email"
+              name=""
+              id="email"
+              className="emailInput"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+              <img src={visibilityIcon} 
+              alt="show password" 
+              className="showPassword" 
+              onClick={()=>setShowPassword((prevState)=>!prevState)}
+              />
+            </div>
+            <Link to='/forgot-password'
+            className='forgotPasswordLink'
+            >
+                Forgot Password
+            </Link>
+            <div className="signInBar">
+                <p className="signInText">
+                    Sign In
+                </p>
+                <button className='signInButton'>
+                    <ArrowRightIcon fill='#ffffff' 
+                    width='34px'
+                    height='34px'></ArrowRightIcon>
+                </button>
+            </div>
+          </form>
+
+          <OAuth></OAuth>
+
+          <Link to='/sign-up' className='registerLink'>
+            Sign Up Instead
+          </Link>
+        </main>
+      </div>
+    </>
+  );
 }
 
-export default SignIn
+export default SignIn;
